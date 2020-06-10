df4d56c6712ced2934f4204d3dbedeea7a9a5b00
skylar-kim <kimsooye@usc.edu>
2020-06-10 12:45:49 -0400

    added moveGoal, getDistance, and other methods

--- src/robot_cleaner.cpp
+++ src/robot_cleaner.cpp
@@ -19,6 +19,8 @@ void rotate(double angular_speed, double relative_angle, bool clockwise);
 double degrees2radians(double angle_in_degrees);
 double setDesiredOrientation(double desired_angle_radians);
 void poseCallback(const turtlesim::Pose::ConstPtr & pose_message);
+void moveGoal(turtlesim::Pose goal_pose, double distance_tolerance);
+double getDistance(double x1, double y1, double x2, double y2);
 
 
 int main(int argc, char **argv) 
@@ -58,12 +60,19 @@ int main(int argc, char **argv)
 	// cin >> clockwise;
 	// rotate(degrees2radians(angular_speed), degrees2radians(angle), clockwise);
 
-	setDesiredOrientation(degrees2radians(120));
+	// setDesiredOrientation(degrees2radians(120));
 	ros::Rate loop_rate(0.5);
+	// loop_rate.sleep();
+	// setDesiredOrientation(degrees2radians(-60));
+	// loop_rate.sleep();
+	// setDesiredOrientation(degrees2radians(0));
+
+	turtlesim::Pose goal_pose;
+	goal_pose.x = 1;
+	goal_pose.y = 1;
+	goal_pose.theta = 0;
+	moveGoal(goal_pose, 0.01);
 	loop_rate.sleep();
-	setDesiredOrientation(degrees2radians(-60));
-	loop_rate.sleep();
-	setDesiredOrientation(degrees2radians(0));
 
 	ros::spin();
 	
@@ -213,3 +222,40 @@ void poseCallback(const turtlesim::Pose::ConstPtr &pose_message)
 	turtlesim_pose.y = pose_message->y;
 	turtlesim_pose.theta = pose_message->theta;
 }
+double getDistance(double x1, double y1, double x2, double y2)
+{
+	return sqrt(pow((x1-x2),2) +pow((y1-y2),2));
+}
+
+void moveGoal(turtlesim::Pose goal_pose, double distance_tolerance)
+{
+	geometry_msgs::Twist vel_msg;
+
+	ros::Rate loop_rate(10);
+	do {
+		/***** Proportional Controller ***/
+		// define linear velocity in the x axis
+		vel_msg.linear.x = 1.5 * getDistance(turtlesim_pose.x, turtlesim_pose.y, goal_pose.x, goal_pose.y);
+		vel_msg.linear.y = 0;
+		vel_msg.linear.z = 0;
+
+		// define angular velocity in the z-axis
+		vel_msg.angular.x = 0;
+		vel_msg.angular.y = 0;
+		vel_msg.angular.z = 4 * (atan2(goal_pose.y - turtlesim_pose.y, goal_pose.x - turtlesim_pose.x)-turtlesim_pose.theta);
+		
+		velocity_publisher.publish(vel_msg);
+
+		ros::spinOnce();
+		loop_rate.sleep();
+	
+	} while (getDistance(turtlesim_pose.x, turtlesim_pose.y, goal_pose.x, goal_pose.y) > distance_tolerance);
+
+	cout << "end move goal" << endl;
+
+	// force stop of the robot
+	vel_msg.linear.x = 0;
+	vel_msg.angular.z = 0;
+	velocity_publisher.publish(vel_msg);
+}
+
